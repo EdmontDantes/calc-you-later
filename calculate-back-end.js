61e3853131b982ae0c42eb5bc2b184e3d8a3bf44
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-05 12:10:40 -0500

    intial commit, 16 passed 16 total

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -1,9 +1,34 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function calculate (num1, num2, operator){
+  if (operator === '+' || operator === 'plus' || operator === 'added to') {
+    
+    return Number(num1) + Number(num2);
+  }
+  else if (operator === '-' || operator === 'minus' || operator === 'subtracted from') {
+
+    return Number(num1) - Number(num2);
+  }
+  else if (operator === 'x' || operator === 'times' || operator === 'X' || operator === 'multiplied by') {
+    
+    return Number(num1) * Number(num2);
+  }
+  else if (operator === '/' || operator === 'divided by') {
+
+    return Number(num1) / Number(num2);
+  }
+  else if (operator === '%' || operator === 'modulus' || operator === 'mod') {
+    
+    return Number(num1) % Number(num2);
+  }
+  else if (operator !== '+' && operator !=='-' && operator !== 'x' && operator !== '/' && operator !== '%') {
+    return "Sorry, that's not a mathematical operation!";
+  }
+
+}
 
 
- 
 
 /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
